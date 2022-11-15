a24ee4d84d3940bea8efac88646b51bfacbb4c40
RashidBaghirov <rashidtb@code.edu.az>
2022-11-16 01:50:07 +0400

    Dont say anything

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//static void Moon()
+//{
+//    Console.WriteLine("bir eded daxil edin");
+//    string numstr = Console.ReadLine();
+//    int number1 = Convert.ToInt32(numstr);
+//    int digits = numstr.Length;
+//    int num = 3;
+//    for (int i = 1; i <= number1; i++)
+//    {
+//        num = num * 10;
+//    }
+//    int sum = number1+num;
+//    Console.WriteLine(sum);
+//}
+//Moon();
+static void Son()
+{
+    string numStr = Console.ReadLine();
+    int num = Convert.ToInt32(numStr);
+    int result = numStr.Length;
+    int main = 3;
+    for (int i = 1; i <= result; i++)
+    {
+        main = main * 10;
+    }
+    int sum = num + main;
+    Console.WriteLine(sum);
+}
+Son();
